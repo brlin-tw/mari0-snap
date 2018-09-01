7a89321abacd054a9dc7e99a3363cb2548e8a341
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2018-09-02 02:14:55 +0800

    snap: Move `version-script` to independent script file
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

---
+++ snap/utilities/set-snap-version.bash
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Program to set snap's version, used by the `version-script` keyword
+# 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com> © 2018
+
+set \
+	-o errexit \
+	-o errtrace \
+	-o nounset \
+	-o pipefail
+
+init(){
+	local \
+		upstream_version \
+		packaging_revision
+
+	upstream_version="$(
+		git \
+			-C parts/my-app-part/src \
+			describe \
+			--always \
+			--dirty=-d \
+			--tags \
+		| sed s/^v//
+	)"
+
+	packaging_revision="$(
+		git \
+			describe \
+			--abbrev=4 \
+			--always \
+			--match nothing \
+			--dirty=-d
+	)"
+
+	printf \
+		-- \
+		'%s' \
+		"${upstream_version}+pkg-${packaging_revision}"
+
+	exit 0
+}
+
+init "${@}"
